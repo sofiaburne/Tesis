bff76f65d038c51c1168f51ef7cec6583ae3819b
sofiaburne <42590422+sofiaburne@users.noreply.github.com>
2018-12-28 09:50:10 -0300

    Update testeo_hipotesisMHD.py
    Mejoré plot, agregué saltos
    Arreglé Machs
    Cálcule v_alf considerando que hay protones y part alfa

--- testeo_hipotesisMHD.py
+++ testeo_hipotesisMHD.py
@@ -6,7 +6,7 @@ MODO_hipotesisMHD = 0
 from mag import shock_date
 from delimitacion_shock import B, t_mag
 from delimitacion_shock import t_swia_mom, densidad_swia, temperatura_swia_norm
-from delimitacion_shock import Bu, Bd, Vu, Vd, iu_v, fu_v, id_v, fd_v
+from delimitacion_shock import Bu, Bd, norm_Bu, norm_Bd, Vu, Vd, i_u, f_u, i_d, f_d, iu_v, fu_v, id_v, fd_v
 from analisis_subestructuras import N
 
 
@@ -29,24 +29,57 @@ if MODO_hipotesisMHD == 1:
     
     #plot de densidad y B para ver que es un shock rapido
     
-    f2, plot1 = plt.subplots()
+    figsize = (30,15)
+    msize = 8
+    lw = 3
+    font_label = 30
+    font_leg = 26
+    ticks_l = 6
+    ticks_w = 3
+    xarrow_B = 9.75
+    xarrow_rho = 9.70
+    xlim_B = np.array([t_mag[3020], t_mag[4244]])
+    xlim_rho = np.array([t_swia_mom[755], t_swia_mom[1060]])
+    ylim_B = 50
+    ylim_rho = 200
     
-    gr1, = plot1.plot(t_mag, B, linewidth = 2, marker ='o', markersize = '4', color = 'C0', label = '$B$')
-    plot1.set_xlabel('Tiempo\n[hora decimal]', fontsize = 30)
-    plot1.set_ylabel('B\n[nT]', fontsize = 30)
-    plt.xlim(t_mag[3020], t_mag[4244])
-    plot1.axes.tick_params(axis = 'both', which = 'both', length = 6, width = 3, labelsize = 30)
-    plot1.axes.grid(axis = 'both', which = 'both', alpha = 0.8, linewidth = 2, linestyle = '--')
+    
+    den_u = np.mean(densidad_swia[iu_v:fu_v])
+    den_d = np.mean(densidad_swia[id_v:fd_v])
+    
+    saltoB = norm_Bd/norm_Bu
+    saltorho = den_d/den_u
+    
+    f2, plot1 = plt.subplots(figsize = figsize)
+    f2.taight_layout = True
+    
+    gr1, = plot1.plot(t_mag, B, linewidth = lw, marker ='o', markersize = msize, color = 'C0', label = '$B$')
+    plot1.axhline(y = norm_Bu, linewidth = lw, color = 'C1')
+    plot1.axhline(y = norm_Bd, linewidth = lw, color = 'C1')
+    plot1.annotate('', xy=(xarrow_B, norm_Bd), xycoords='data', xytext=(xarrow_B, norm_Bu), textcoords='data', arrowprops=dict(arrowstyle='<->', connectionstyle='arc3', color='C1', lw=lw))
+    plot1.text(xarrow_B-0.01, (norm_Bd + norm_Bu)/2, '$\Delta$B = {}'.format(int(saltoB)), rotation = 90, verticalalignment='center', fontsize = font_leg, color = 'C1', bbox=dict(facecolor='white', edgecolor='None', alpha=1))
+    plot1.set_xlabel('Tiempo\n[hora decimal]', fontsize = font_label)
+    plot1.set_ylabel('B\n[nT]', fontsize = font_label)
+    plt.xlim(xlim_B)
+    plt.ylim(ymax = ylim_B)
+    plot1.axes.tick_params(axis = 'both', which = 'both', length = ticks_l, width = ticks_w, labelsize = font_label)
+    plot1.axes.grid(axis = 'both', which = 'both', alpha = 0.8, linewidth = lw, linestyle = '--')
+    
     
     plot2 = plt.twinx(plot1)
-    gr2, = plot2.plot(t_swia_mom, densidad_swia, linewidth = 2, marker ='o', markersize = '4',  color = 'C2', label = '$n_p$')
-    plt.xlim(t_swia_mom[755], t_swia_mom[1060])
-    plot2.set_ylabel('$n_p$\n[$cm^{-3}$]', fontsize = 30)
-    plot2.axes.tick_params(axis = 'y', which = 'both', length = 6, width = 3, labelsize = 30)
+    gr2, = plot2.plot(t_swia_mom, densidad_swia, linewidth = lw, marker ='o', markersize = msize,  color = 'C2', label = '$n_p$')
+    plot2.axhline(y = den_u, linewidth = lw, color = 'C3')
+    plot2.axhline(y = den_d, linewidth = lw, color = 'C3')
+    plt.annotate('', xy=(xarrow_rho, den_d), xycoords='data', xytext=(xarrow_rho, den_u), textcoords='data', arrowprops=dict(arrowstyle='<->', connectionstyle='arc3', color='C3', lw=lw))
+    plt.text(xarrow_rho-0.01, (den_d + den_u)/2, '$\Delta$n = {}'.format(int(saltorho)), rotation = 90, verticalalignment='center', fontsize = font_leg, color = 'C3', bbox=dict(facecolor='white', edgecolor='None', alpha=1))
+    plt.xlim(xlim_rho)
+    plt.ylim(ymax = ylim_rho)
+    plot2.set_ylabel('$n_p$\n[$cm^{-3}$]', fontsize = font_label)
+    plot2.axes.tick_params(axis = 'y', which = 'both', length = ticks_l, width = ticks_w, labelsize = font_label)
     
-    plot1.legend(handles = [gr1,gr2], loc = 0, fontsize = 20)
+    plot1.legend(handles = [gr1,gr2], loc = 0, fontsize = font_leg)
     
-    f2.savefig(path_analisis+'fast_shock_{}'.format(shock_date))
+    #f2.savefig(path_analisis+'fast_shock_{}'.format(shock_date))
 
 #%%
 
@@ -77,8 +110,8 @@ B_dt = (B_d - B_dn*norm)
 #densidad en kg/m^3
 mp = 1.67e-27 #masa del proton en kg
 #mp = 1.5e-10 #masa del proton en joules/c^2
-densnum_u = np.mean(densidad_swia[iu_v:fu_v])*(1e-6) #1/m^3
-densnum_d = np.mean(densidad_swia[id_v:fd_v])*(1e-6) #1/m^3
+densnum_u = np.mean(densidad_swia[iu_v:fu_v])*(1e6) #1/m^3
+densnum_d = np.mean(densidad_swia[id_v:fd_v])*(1e6) #1/m^3
 rho_u = mp*densnum_u
 rho_d = mp*densnum_d
 
@@ -94,92 +127,78 @@ Pd = densnum_d*kB*Td
 #numeros de Mach
 
 mu = (np.pi*4)*(1e-7) #permeabilidad mag del vacio en Wb/Am=mT/A
-v_alfv = (np.linalg.norm(B_u)/np.sqrt(mu*rho_u))*(1e-3) # km/s
-v_cs = (np.sqrt((Pu/rho_u)*(5/3)))*(1e-3) # km/s
-#v_cs = ((5/3)*(kB*Tu)/mp)*(1e-3) #km/s
 
-M_A = np.linalg.norm(Vu)/v_alfv
-M_cs = np.linalg.norm(Vu)/v_cs
-M_f = np.linalg.norm(Vu)/np.sqrt(v_alfv**2 + v_cs**2)
+v_alfv = (np.linalg.norm(B_u)/np.sqrt(mu*rho_u))*(1e-3) # km/s
+v_alfv_2 = (np.linalg.norm(B_u)/np.sqrt(mu*mp*(densnum_u + 0.05*densnum_u)))*(1e-3) # km/s   considero n=n_p+n_He, con n_He = 5%n_p
+
+v_cs = (np.sqrt((Pu/(2*rho_u))*(5/3)))*(1e-3) # km/s
+#si no les aplico sqrt dan ordenes de magnitud sin sentido:
+#v_cs_2 = ((5/3)*(kB*Tu)/mp)*(1e-3) #km/s
+#v_cs_3 = ((3*kB*Tu/2)/mp)*(1e-3) #km/s
 
 M_c = 2.7 #M_A critico para theta_Bun = 90, para angulos menores decrese
+M_A = np.linalg.norm(Vu)/v_alfv_2
+M_cs = np.linalg.norm(Vu)/v_cs
+M_f = np.linalg.norm(Vu)/np.sqrt(v_alfv_2**2 + v_cs**2)
+
+#Mach locales
+M_A_loc = np.abs(np.dot(Vu,norm))/v_alfv_2
+M_cs_loc = np.abs(np.dot(Vu,norm))/v_cs
+M_f_loc = np.abs(np.dot(Vu,norm))/np.sqrt(v_alfv_2**2 + v_cs**2)
+
 
 #beta del plasma upstream
 beta = Pu/(B_u**2/2*mu)
 
-'''
-chequeo si con la presion y densidad anterior
-se cumple la hipotesis de evolucion adiabatica
-(chequeo si gamma da 5/3)
-'''
+
+#chequeo si se cumple la hipotesis de evolucion adiabatica (gamma da 5/3)
 G = Symbol('G')
 eq_adiab = Pu*rho_u**G - (Pd*rho_d**G)
 gam = solve(eq_adiab, G)
+
+
+
 
 #relaciones RH en porcentaje (100 = se cumple perfectamente)
 
 #conservacion de la masa
 cons_masa_u = np.abs(rho_u*U_un)
 cons_masa_d = np.abs(rho_d*U_dn)
-
-if cons_masa_u > cons_masa_d:
-    cons_masa = cons_masa_d/cons_masa_u*100
-else:
-    cons_masa = cons_masa_u/cons_masa_d*100    
+cons_masa = np.min([cons_masa_u,cons_masa_d])/np.max([cons_masa_u,cons_masa_d])*100
 
 #consevacion del impulso normal al shock
 cons_impul_n_u = np.abs(rho_u*U_un**2 + Pu + B_u**2/(2*mu))
 cons_impul_n_d = np.abs(rho_d*U_dn**2 + Pd + B_d**2/(2*mu))
-
 cons_impul_n = np.empty_like(cons_impul_n_u)
 for i in range(len(cons_impul_n_u)):
-    if cons_impul_n_u[i] > cons_impul_n_d[i]:
-        cons_impul_n[i] = cons_impul_n_d[i]/cons_impul_n_u[i]*100
-    else:
-        cons_impul_n[i] = cons_impul_n_u[i]/cons_impul_n_d[i]*100
+    cons_impul_n[i] = np.min([cons_impul_n_d[i],cons_impul_n_u[i]])/np.max([cons_impul_n_d[i],cons_impul_n_u[i]])*100
 
 #conservacion del impulso tangencial al shock
 cons_impul_t_u = np.abs(rho_u*U_un*U_ut - B_un/mu*B_ut)
 cons_impul_t_d = np.abs(rho_d*U_dn*U_dt - B_dn/mu*B_dt)
-
 cons_impul_t = np.empty_like(cons_impul_t_u)
 for i in range(len(cons_impul_t_u)):
-    if cons_impul_t_u[i] > cons_impul_t_d[i]:
-        cons_impul_t[i] = cons_impul_t_d[i]/cons_impul_t_u[i]*100
-    else:
-        cons_impul_t[i] = cons_impul_t_u[i]/cons_impul_t_d[i]*100
+    cons_impul_t[i] = np.min([cons_impul_t_d[i],cons_impul_t_u[i]])/np.max([cons_impul_t_d[i],cons_impul_t_u[i]])*100
 
 #consevacion de la energia
 gamma = 5/3
 cons_energ_u = np.abs(rho_u*U_un*(1/2*U_u**2 + gamma/(gamma-1)*Pu/rho_u) + U_un*B_u**2/mu - np.dot(U_u,B_u)*B_un/mu)
 cons_energ_d = np.abs(rho_d*U_dn*(1/2*U_d**2 + gamma/(gamma-1)*Pd/rho_d) + U_dn*B_d**2/mu - np.dot(U_d,B_d)*B_dn/mu)
-
 cons_energ = np.empty_like(cons_energ_u)
 for i in range(len(cons_energ)):
-    if cons_energ_u[i] > cons_energ_d[i]:
-        cons_energ[i] = cons_energ_d[i]/cons_energ_u[i]*100
-    else:
-        cons_energ[i] = cons_energ_u[i]/cons_energ_d[i]*100
+    cons_energ[i] = np.min([cons_energ_d[i],cons_energ_u[i]])/np.max([cons_energ_d[i],cons_energ_u[i]])*100
 
 #conservacion de componente normal de B
 cons_Bn_u = np.abs(B_un)
 cons_Bn_d = np.abs(B_dn)
-
-if cons_Bn_u > cons_Bn_d:
-    cons_Bn = cons_Bn_d/cons_Bn_u*100
-else:
-    cons_Bn = cons_Bn_u/cons_Bn_d*100
+cons_Bn = np.min([cons_Bn_d,cons_Bn_u])/np.max([cons_Bn_d,cons_Bn_u])*100
 
 #conservacion de campo electrico tang
 cons_Et_u = np.abs(U_un*B_ut - B_un*U_ut)
 cons_Et_d = np.abs(U_dn*B_dt - B_dn*U_dt)
-
 cons_Et = np.empty_like(cons_Et_u)
 for i in range(len(cons_Et)):
-    if cons_Et_u[i] > cons_Et_d[i]:
-        cons_Et[i] = cons_Et_d[i]/cons_Et_u[i]*100
-    else:
-        cons_Et[i] = cons_Et_u[i]/cons_Et_d[i]*100
+     cons_Et[i] = np.min([cons_Et_d[i],cons_Et_u[i]])/np.max([cons_Et_d[i],cons_Et_u[i]])*100
 
 #hipotesis de coplanaridad
 hipt_copl_B = np.dot(norm,np.cross(B_u,B_d))
@@ -189,40 +208,57 @@ hipt_copl_B = np.dot(norm,np.cross(B_u,B_d))
 
 if MODO_hipotesisMHD == 1:
     
-    datos6 = np.zeros([10,5])
+    datos6 = np.zeros([14,5])
     
     #normal de referencia
     datos6[0,0:3] = norm
     
-    #Tu Td Pu Pd
+    #Tu Td Pu Pd rho_u rho_d
     datos6[1,0] = Tu
     datos6[1,1] = Td
     datos6[1,2] = Pu
     datos6[1,3] = Pd
+    datos6[1,4] = rho_u
+    datos6[1,4] = rho_d
+    
+    #gamma de evolución adiabatica
+    datos6[2,0] = gam
+    
+    #velocidades
+    datos6[3,0] = v_alfv
+    datos6[3,1] = v_cs
+    datos6[3,2] = v_cs_2
+    datos6[3,3] = v_cs_3
     
     #numeros de Mach
-    datos6[2,0] = M_A
-    datos6[2,1] = M_cs
-    datos6[2,2] = M_f
-    datos6[2,3] = M_c
+    datos6[4,0] = M_A
+    datos6[4,1] = M_cs
+    datos6[4,2] = M_f
+    datos6[4,3] = M_c
     
     #beta upstream
-    datos6[3,0] = beta
+    datos6[5,0] = beta
     
     #conservaciones
-    datos6[4,0] = cons_masa
-    datos6[5,0:3] = cons_impul_n
-    datos6[6,0:3] = cons_impul_t
-    datos6[7,0:3] = cons_energ
-    datos6[8,0] = cons_Bn
-    datos6[9,0:3] = cons_Et
+    datos6[6,0] = cons_masa
+    datos6[7,0:3] = cons_impul_n
+    datos6[8,0:3] = cons_impul_t
+    datos6[9,0:3] = cons_energ
+    datos6[10,0] = cons_Bn
+    datos6[11,0:3] = cons_Et
     
     #hipotesis teo coplanaridad
-    datos6[10,0] = hipt_copl_B
+    datos6[12,0] = hipt_copl_B
+    
+    #salto B, salto rho
+    datos6[13,0] = saltoB
+    datos6[13,1] = saltorho
     
     np.savetxt(path_analisis+'hipotesis_MHD_shock_{}'.format(shock_date), datos6, delimiter = '\t',
                header = '\n'.join(['{}'.format(shock_date),'normal de ref usada en calculos',
-                                                     'Tu Td [K] Pu Pd [Pa]',
+                                                     'Tu Td [K] Pu Pd [Pa], rho_u rho_d [kg/m^3]',
+                                                     'gamma si el sist evolucionara adiabaticamente',
+                                                     'v_alf v_cs v_cs_2 v_cs_3',
                                                      'M_Alfv M_sonico M_rapido M_critico', 'beta',
                                                      'conservacion masa',
                                                      'conservacion impulso norm',
@@ -230,4 +266,5 @@ if MODO_hipotesisMHD == 1:
                                                      'conservacion energia',
                                                      'conservacion Bn',
                                                      'conservacion campo electrico tang',
-                                                     'hipotesis teo coplanaridad [nT]']))
+                                                     'hipotesis teo coplanaridad [nT]',
+                                                     'salto B salto rho']))
